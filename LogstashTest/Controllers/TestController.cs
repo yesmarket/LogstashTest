c690e2c5927016d95b821eb79ca2d8973a5d2998
Ryan Bartsch <rbartsch@adam.com.au>
2018-08-20 06:25:22 +0930

    tiedied up test controller

--- LogstashTest/Controllers/TestController.cs
+++ LogstashTest/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
+using Serilog.Context;
 using Serilog.Sinks.Elasticsearch;
 
 namespace LogstashTest.Controllers
@@ -9,65 +10,62 @@ namespace LogstashTest.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
+        public const string FLAT_FORMAT = "{Timestamp:HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Properties}{NewLine}";
+
         // GET api/values
         [HttpGet]
-        [Route("ElasticsearchDirect")]
+        [Route("f")]
+        public ActionResult File()
+        {
+            Log(lc => lc.WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, outputTemplate: FLAT_FORMAT), "Logging directly to File");
+            return Ok();
+        }
+
+        // GET api/values
+        [HttpGet]
+        [Route("es")]
         public ActionResult ElasticsearchDirect()
         {
-            using (var log = new LoggerConfiguration().WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))).CreateLogger())
-            {
-                var loggingDirectlyToEs = "Logging directly to Elasticsearch";
-                log.Information(loggingDirectlyToEs);
-                log.Warning(loggingDirectlyToEs);
-                log.Error(loggingDirectlyToEs);
-            }
-
+            Log(lc => lc.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))), "Logging directly to Elasticsearch");
             return Ok();
         }
 
         [HttpGet]
-        [Route("ElasticsearchViaDockerProvider")]
+        [Route("es2")]
         public ActionResult ElasticsearchViaDockerProvider()
         {
-            using (var log = new LoggerConfiguration().WriteTo.Console().CreateLogger())
-            {
-                const string loggingToEsViaDockerProvider = "Logging to Elasticsearch via docker provider";
-                log.Information(loggingToEsViaDockerProvider);
-                log.Warning(loggingToEsViaDockerProvider);
-                log.Error(loggingToEsViaDockerProvider);
-            }
-
+            Log(lc => lc.WriteTo.Console(outputTemplate: FLAT_FORMAT), "Logging to Elasticsearch via docker provider");
             return Ok();
         }
 
         [HttpGet]
-        [Route("LogstashDirect")]
+        [Route("ls")]
         public ActionResult LogstashDirect()
         {
-            using (var log = new LoggerConfiguration().WriteTo.LogstashHttp("https://elk-host:8443").CreateLogger())
-            {
-                const string loggingDirectlyToLs = "Logging directly to Logstash";
-                log.Information(loggingDirectlyToLs);
-                log.Warning(loggingDirectlyToLs);
-                log.Error(loggingDirectlyToLs);
-            }
-
+            Log(lc => lc.WriteTo.LogstashHttp("https://elk-host:8443"), "Logging directly to Logstash");
             return Ok();
         }
 
         [HttpGet]
-        [Route("LogstashViaDockerProvider")]
+        [Route("ls2")]
         public ActionResult LogstashViaDockerProvider()
         {
-            using (var log = new LoggerConfiguration().WriteTo.Console().CreateLogger())
-            {
-                const string loggingToLsViaDockerProvider = "Logging to Logstash via docker provider";
-                log.Information(loggingToLsViaDockerProvider);
-                log.Warning(loggingToLsViaDockerProvider);
-                log.Error(loggingToLsViaDockerProvider);
-            }
-
+            Log(lc => lc.WriteTo.Console(outputTemplate: FLAT_FORMAT), "Logging to Logstash via docker provider");
             return Ok();
+        }
+
+        private void Log(Func<LoggerConfiguration, LoggerConfiguration> func, string message)
+        {
+            var loggerCoonfiguration = new LoggerConfiguration().Enrich.FromLogContext();
+            using (var log = func.Invoke(loggerCoonfiguration).CreateLogger())
+            {
+                using (LogContext.PushProperty("conversationId", Guid.NewGuid()))
+                {
+                    log.Information(message);
+                    log.Warning(message);
+                    log.Error(message);
+                }
+            }
         }
     }
 }
